1b3347599b607f625df952deb7e3e29ffba7c5d5
SudhariPerera <Sudhari08sudh@gmail.com>
2026-05-20 16:14:48 +1200

    15

---
+++ SD7501Bulky.Models/ViewModels/OrderVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SD7501Bulky.Models.ViewModels
+{
+    public class OrderVM
+    {
+        public OrderHeader OrderHeader { get; set; }
+        public IEnumerable<OrderDetail> OrderDetails { get; set; }
+    }
+}
