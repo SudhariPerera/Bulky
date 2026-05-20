b5018b905c9748ad560434247ca3c648b6fe73ee
SudhariPerera <Sudhari08sudh@gmail.com>
2026-05-20 17:49:03 +1200

    OrderManagement part1

--- BulkyWeb/wwwroot/js/order.js
+++ BulkyWeb/wwwroot/js/order.js
@@ -4,24 +4,24 @@ $(document).ready(function () {
     loadDataTable();
 });
 
-function loadDataTable() {
+function loadDataTable(status) {
     dataTable = $('#tblData').DataTable({
-        "ajax": { url: '/admin/order/getall' },
+        "ajax": { url: '/admin/order/getall?status=' + status },
         "columns": [
-            { data: 'id', "width": "25%" },
-            { data: 'name', "width": "15%" },
-            { data: 'phoneNumber', "width": "10%" },
-            { data: 'applicationUser.email', "width": "15%" },
+            { data: 'id', "width": "5%" },
+            { data: 'name', "width": "25%" },
+            { data: 'phoneNumber', "width": "20%" },
+            { data: 'applicationUser.email', "width": "20%" },
             { data: 'orderStatus', "width": "10%" },
             { data: 'orderTotal', "width": "10%" },
             {
                 data: 'id',
                 "render": function (data) {
                     return `<div class="w-75 btn-group" role="group">
-                     <a href="/admin/order/upsert?id=${data}" class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i> Edit</a>               
-                    </div>`
+                 <a href="/admin/order/details?orderId=${data}" class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i> </a>               
+                 </div>`
                 },
-                "width": "25%"
+                "width": "10%"
             }
         ]
     });
--- SD7501Bulky.Models/ViewModels/OrderVM.cs
+++ SD7501Bulky.Models/ViewModels/OrderVM.cs
@@ -9,6 +9,6 @@ namespace SD7501Bulky.Models.ViewModels
     public class OrderVM
     {
         public OrderHeader OrderHeader { get; set; }
-        public IEnumerable<OrderDetail> OrderDetails { get; set; }
+        public IEnumerable<OrderDetail> OrderDetail { get; set; }
     }
 }
