704fcac9993ee994e367b915241babd486c851a4
SudhariPerera <Sudhari08sudh@gmail.com>
2026-05-20 16:35:10 +1200

    17

--- BulkyWeb/wwwroot/js/order.js
+++ BulkyWeb/wwwroot/js/order.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
 
 function loadDataTable() {
     dataTable = $('#tblData').DataTable({
-        "ajax": { url: '/admin/product/getall' },
+        "ajax": { url: '/admin/order/getall' },
         "columns": [
             { data: 'id', "width": "25%" },
             { data: 'name', "width": "15%" },
@@ -18,7 +18,7 @@ function loadDataTable() {
                 data: 'id',
                 "render": function (data) {
                     return `<div class="w-75 btn-group" role="group">
-                     <a href="/admin/product/upsert?id=${data}" class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i> Edit</a>               
+                     <a href="/admin/order/upsert?id=${data}" class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i> Edit</a>               
                     </div>`
                 },
                 "width": "25%"
