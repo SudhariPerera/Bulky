ba10b4afdfd2444c25c3874a9f800c74f2d11db2
SudhariPerera <Sudhari08sudh@gmail.com>
2026-05-20 15:57:47 +1200

    10

--- BulkyWeb/Areas/Customer/Controllers/CartController.cs
+++ BulkyWeb/Areas/Customer/Controllers/CartController.cs
@@ -134,7 +134,7 @@ namespace BulkyWeb.Areas.Customer.Controllers
 
             if (applicationUser.CompanyId.GetValueOrDefault() == 0)
             {
-                var domain = "https://localhost:7051/";
+                var domain = "https://localhost:7092/";
                 var options = new SessionCreateOptions
                 {
                     SuccessUrl = domain + $"customer/cart/OrderConfirmation?id={ShoppingCartVM.OrderHeader.Id}",
